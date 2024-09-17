7a437ebb09e9b9fc6d89dd2342f3eaba9ab1699c
Miron <miron.nicolaevich@gmail.com>
2024-09-17 21:00:24 +0200

    two_sum

---
+++ algorithms/two_sum.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        num_to_index = {}
+
+        for index, num in enumerate(nums):
+            complement = target - num
+            if complement in num_to_index:
+                return [num_to_index[complement], index]
+            num_to_index[num] = index
+
+        return []
+
+
+nums_ = [2, 5, 1, 17]
+d = Solution()
+for i in 7, 6, 19:
+    print(d.twoSum(nums=nums_, target=i))
