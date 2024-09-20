deb05f8621bf983a87b03bf46064c3b5acd92adb
Miron <miron.nicolaevich@gmail.com>
2024-09-20 22:23:19 +0200

    rising_temperature

--- sql/rising_temperature.sql
+++ sql/rising_temperature.sql
@@ -5,5 +5,5 @@ FROM Weather AS w1
 WHERE w1.temperature > (
   SELECT MAX(w2.temperature)
   FROM Weather AS w2
-  WHERE w2.recordDate < w1.recordDate
+  WHERE (w1.recordDate - w2.recordDate) = 1
 );
