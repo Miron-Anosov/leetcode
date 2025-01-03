700f02546f1ca3a06ac359fefd4e05fa0c7a8cf5
Miron-Anosov <mairon26rus@gmail.com>
2025-01-03 14:03:17 +0100

    https://leetcode.com/problems/length-of-last-word/description/

---
+++ algorithms/58_length_of_last_word.py
@@ -0,0 +1,61 @@
+"""https://leetcode.com/problems/length-of-last-word/description/"""
+
+"""
+Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal
+substring
+consisting of non-space characters only.
+
+
+
+Example 1:
+
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+
+Example 2:
+
+Input: s = "   fly me   to   the moon  "
+Output: 4
+Explanation: The last word is "moon" with length 4.
+
+Example 3:
+
+Input: s = "luffy is still joyboy"
+Output: 6
+Explanation: The last word is "joyboy" with length 6.
+
+
+
+Constraints:
+
+    1 <= s.length <= 104
+    s consists of only English letters and spaces ' '.
+    There will be at least one word in s.
+
+
+"""
+
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        count: int = 0
+        for i in s[::-1]:
+            if i.isalpha():
+                count += 1
+            elif not i.isalpha() and not count:
+                continue
+            else:
+                return count
+        else:
+            return count
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    assert sol.lengthOfLastWord("   fly me   to   the moon  ") == 4
+    assert sol.lengthOfLastWord("Hello World") == 5
+    assert sol.lengthOfLastWord("a") == 1
+    assert sol.lengthOfLastWord("luffy is still joyboy") == 6
